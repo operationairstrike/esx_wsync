5ee924e7563ace3bd9348336dde58e40bf45c327
Sadler2 <sad2004@mail.ru>
2019-07-21 02:42:42 +0600

    Add common utility functions

--- __resource.lua
+++ __resource.lua
@@ -1,9 +1,11 @@
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
 client_scripts {
+    'util.lua',
     'vs_client.lua',
 }
 
 server_scripts {
+    'util.lua',
     'vs_server.lua',
 }
